1c144b9abc6ec1b9bdcc263bf55620ebf4b61271
AboneyimANAN <80243136+AboneyimANAN@users.noreply.github.com>
2021-03-24 20:14:37 +0300

    Update DiscordPresence.java

--- src/main/java/me/earth/phobos/DiscordPresence.java
+++ src/main/java/me/earth/phobos/DiscordPresence.java
@@ -21,12 +21,12 @@ public class DiscordPresence {
 
     public static void start() {
         DiscordEventHandlers handlers = new DiscordEventHandlers();
-        rpc.Discord_Initialize("737779695134834695", handlers, true, "");
+        rpc.Discord_Initialize("824330111414960149", handlers, true, "");
         DiscordPresence.presence.startTimestamp = System.currentTimeMillis() / 1000L;
         DiscordPresence.presence.details = Minecraft.getMinecraft().currentScreen instanceof GuiMainMenu ? "In the main menu." : "Playing " + (Minecraft.getMinecraft().currentServerData != null ? (RPC.INSTANCE.showIP.getValue().booleanValue() ? "on " + Minecraft.getMinecraft().currentServerData.serverIP + "." : " multiplayer.") : " singleplayer.");
         DiscordPresence.presence.state = RPC.INSTANCE.state.getValue();
-        DiscordPresence.presence.largeImageKey = "phobos";
-        DiscordPresence.presence.largeImageText = "Phobos 1.9.0";
+        DiscordPresence.presence.largeImageKey = "beta";
+        DiscordPresence.presence.largeImageText = "BeTa Hack";
         rpc.Discord_UpdatePresence(presence);
         thread = new Thread(() -> {
             while (!Thread.currentThread().isInterrupted()) {
